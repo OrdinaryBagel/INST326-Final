207599787973c19759fb71f5eae7b957fe7345b3
OrdinaryBagel <159103544+OrdinaryBagel@users.noreply.github.com>
2025-04-20 21:17:32 -0400

    Create Basketball_predictions.py
    Created the Basketball_predictions file and added the team class, its init method, and an update method.

---
+++ Basketball_predictions.py
@@ -0,0 +1,91 @@
+
+
+teamsDict = {}
+
+class Team:
+    def __init__(self,name, gp, pts, off_rebounds, def_rebounds, ast, stl, blk, to, pf):
+        """
+        Driver: Thomas
+        Navigator: Khalid
+        initalizes team objects the variables represent:
+        name = name
+        gp = games played
+        pts = points
+        off_rebounds = offensive rebounds
+        def_rebounds = defensive rebounds
+        total_rebounds = total rebounds
+        ast = assists
+        stl = steals
+        blk = blocks
+        to = turn overs
+        pf = fouls
+        ast_to = assist turnover ratio"""
+        self.name = name
+        self.gp = gp
+        self.pts = pts
+        self.off_rebounds = off_rebounds
+        self.def_rebounds = def_rebounds
+        self.total_rebounds = off_rebounds + def_rebounds
+        self.ast = ast
+        self.stl = stl
+        self.blk = blk
+        self.to = to
+        self.pf = pf
+        self.ast_to = ast/to
+    def update(self, gp, pts, off_rebounds, def_rebounds, ast, stl, blk, to, pf):
+        """
+        updates the stats of a team, basically the same as __init__ so it will not be counted for total methods"""
+        self.gp = gp
+        self.pts = pts
+        self.off_rebounds = off_rebounds
+        self.def_rebounds = def_rebounds
+        self.total_rebounds = off_rebounds + def_rebounds
+        self.ast = ast
+        self.stl = stl
+        self.blk = blk
+        self.to = to
+        self.pf = pf
+        self.ast_to = ast/to
+
+def functions(func):
+    """determines what the program will do, will be update by multiple people as the project moves forward."""
+    if func == 1:
+        """creates new teams"""
+        name = input("Please enter the name of the team: ")
+        gp = int(input(f"Please enter the number of games played by the {name}: "))
+        pts = int(input(f"Please enter the average number of points per game scored by the {name}: "))
+        off_rebounds = int(input(f"Please enter the average number of offensive rebounds per game earned by the {name}: "))
+        def_rebounds = int(input(f"Please enter the average number of defensive rebounds per game earned by the {name}: "))
+        ast = int(input(f"Please enter the average number of assists per game earned by the {name}: "))
+        stl = int(input(f"Please enter the average number of steals per game earned by the {name}: "))
+        blk = int(input(f"Please enter the average number of blocks per game earned by the {name}: "))
+        to = int(input(f"Please enter the average number of turnovers per game earned by the {name}: "))
+        pf = int(input(f"Please enter the average number of fouls per game earned by the {name}: "))
+        teamsDict[name] = Team(name,gp,pts,off_rebounds,def_rebounds,ast,stl,blk,to,pf)
+    elif func == 2:
+        """updates existing teams"""
+        name = input("Please enter the name of the team: ")
+        if name in teamsDict:
+            gp = int(input(f"Please enter the number of games played by the {name}: "))
+            pts = int(input(f"Please enter the average number of points per game scored by the {name}: "))
+            off_rebounds = int(input(f"Please enter the average number of offensive rebounds per game earned by the {name}: "))
+            def_rebounds = int(input(f"Please enter the average number of defensive rebounds per game earned by the {name}: "))
+            ast = int(input(f"Please enter the average number of assists per game earned by the {name}: "))
+            stl = int(input(f"Please enter the average number of steals per game earned by the {name}: "))
+            blk = int(input(f"Please enter the average number of blocks per game earned by the {name}: "))
+            to = int(input(f"Please enter the average number of turnovers per game earned by the {name}: "))
+            pf = int(input(f"Please enter the average number of fouls per game earned by the {name}: "))
+        else:
+            print("That team does not exist")
+    elif func == 0:
+        """ends the program"""
+        return 0
+    main()
+
+def main():
+    func = int(input("Please type a number to select what you want to do.\n1:Create new team\n2:Update team\n0:Exit the program\n"))
+    functions(func)
+
+if __name__=="__main__":
+    main()
+    
