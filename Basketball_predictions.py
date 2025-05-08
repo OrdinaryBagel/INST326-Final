0b7cb3416c1a5226cbef316fbb8de6aa4a5bb21f
OrdinaryBagel <159103544+OrdinaryBagel@users.noreply.github.com>
2025-05-08 02:51:47 -0400

    Changed Adding Teams, Added a csv
    Changed adding teams to only work with a proper CSV file, Adds a full team of players. Also added 1 CSV file that represents a full team

--- Basketball_predictions.py
+++ Basketball_predictions.py
@@ -1,97 +1,120 @@
-
+import pandas as pd
 
 teamsDict = {}
 
 class Team:
-    def __init__(self,name, gp, pts, off_rebounds, def_rebounds, ast, stl, blk, to, pf):
+    def __init__(self,name, csv):
         """
         Driver: Thomas, Peter
         Navigator: Khalid
-        initalizes team objects the variables represent:
+        initalizes team objects using pandas and a csv containg team stats
+        (the csv must be formatted like the one in our github)
+        the variables represent:
         name = name
         gp = games played
         pts = points
-        off_rebounds = offensive rebounds
-        def_rebounds = defensive rebounds
-        total_rebounds = total rebounds
+        rb = rebounds
         ast = assists
         stl = steals
         blk = blocks
         to = turn overs
-        pf = fouls
-        ast_to = assist turnover ratio"""
+        pf = fouls"""
         self.name = name
-        self.gp = gp
-        self.pts = pts
-        self.off_rebounds = off_rebounds
-        self.def_rebounds = def_rebounds
-        self.total_rebounds = off_rebounds + def_rebounds
-        self.ast = ast
-        self.stl = stl
-        self.blk = blk
-        self.to = to
-        self.pf = pf
-        self.ast_to = ast/to
-    def update(self, gp, pts, off_rebounds, def_rebounds, ast, stl, blk, to, pf):
+        self.pts = 0
+        self.rb = 0
+        self.ast = 0
+        self.stl = 0
+        self.blk = 0
+        self.to = 0
+        self.pf = 0
+        self.playersDict = {}
+        teamData = pd.read_csv(csv)
+        for i in range(0,len(teamData.index)):
+            p_name = teamData["Player"][i]
+            p_gp = teamData["GP"][i]
+            p_rb = teamData["Reb/G"][i]
+            p_ast = teamData["APG"][i]
+            p_stl = teamData["SPG"][i]
+            p_blk = teamData["BPG"][i]
+            p_to = teamData["TO"][i]
+            p_pf = teamData["PF"][i]
+            p_pts = teamData["PPG"][i]
+            self.playersDict[p_name] = Player(p_name, p_gp, p_pts, p_rb, p_ast, p_stl, p_blk, p_to, p_pf)
+        self.calc_team_total()
+
+    def calc_team_total(self):
         """
-        updates the stats of a team, basically the same as __init__ so it will not be counted for total methods"""
-        self.gp = gp
-        self.pts = pts
-        self.off_rebounds = off_rebounds
-        self.def_rebounds = def_rebounds
-        self.total_rebounds = off_rebounds + def_rebounds
-        self.ast = ast
-        self.stl = stl
-        self.blk = blk
-        self.to = to
-        self.pf = pf
-        self.ast_to = ast/to
+        Driver:Thomas
+        Navigator:Peter
+        Calculates the total of the players stats to get the team stats
+        """
+        for key in self.playersDict:
+            self.pts += self.playersDict[key].pts
+            self.rb += self.playersDict[key].rb
+            self.ast += self.playersDict[key].ast
+            self.stl += self.playersDict[key].stl
+            self.blk += self.playersDict[key].blk
+            self.to += self.playersDict[key].to
+            self.pf += self.playersDict[key].pf
+    def print_player(self,name):
+        print(self.playersDict[name])
+    def update(self):
+        """
+        Should be able to call other methods that add players, remove players, update a players stats, and trade players.
+        After any of these are done the total stats should be updated to reflect it.
+        """
 
     def __repr__(self):
         return (
-            f"Team{self.name}, GP={self.gp}, PTS={self.pts}\n"
-            f"REB={self.total_rebounds} OFF={self.off_rebounds}, DEF={self.def_rebounds}\n"
+            f"Team: {self.name}, PTS={self.pts}\n"
+            f"REB={self.rb}\n"
             f"AST={self.ast}, STL={self.stl}, BLK={self.blk}\n"
-            f"TO={self.to}, PF={self.pf}, AST/TO={self.ast_to}\n"
+            f"TO={self.to}, PF={self.pf}\n"
         )
-    
+
+class Player:
+    def __init__(self, name, gp, pts, rb, ast, stl, blk, to, pf):
+        """
+        initializes the player object
+        """
+        self.name = name
+        self.gp = gp
+        self.pts = pts
+        self.rb = rb
+        self.ast = ast
+        self.stl = stl
+        self.blk = blk
+        self.to = to
+        self.pf = pf
+    def __repr__(self):
+        return (
+            f"Player: {self.name}, GP={self.gp}, PTS={self.pts}\n"
+            f"REB={self.rb}\n"
+            f"AST={self.ast}, STL={self.stl}, BLK={self.blk}\n"
+            f"TO={self.to}, PF={self.pf}\n"
+        )
+
 def functions(func):
     """determines what the program will do, will be update by multiple people as the project moves forward."""
     if func == 1:
         """creates new teams"""
         name = input("Please enter the name of the team: ")
-        gp = float(input(f"Please enter the number of games played by the {name}: "))
-        pts = float(input(f"Please enter the average number of points per game scored by the {name}: "))
-        off_rebounds = float(input(f"Please enter the average number of offensive rebounds per game earned by the {name}: "))
-        def_rebounds = float(input(f"Please enter the average number of defensive rebounds per game earned by the {name}: "))
-        ast = float(input(f"Please enter the average number of assists per game earned by the {name}: "))
-        stl = float(input(f"Please enter the average number of steals per game earned by the {name}: "))
-        blk = float(input(f"Please enter the average number of blocks per game earned by the {name}: "))
-        to = float(input(f"Please enter the average number of turnovers per game earned by the {name}: "))
-        pf = float(input(f"Please enter the average number of fouls per game earned by the {name}: "))
-        teamsDict[name] = Team(name,gp,pts,off_rebounds,def_rebounds,ast,stl,blk,to,pf)
+        csv = input("Please enter the CSV file containg the teams information: ")
+        teamsDict[name] = Team(name,csv)
     elif func == 2:
-        """updates existing teams"""
         name = input("Please enter the name of the team: ")
-        if name in teamsDict:
-            gp = float(input(f"Please enter the number of games played by the {name}: "))
-            pts = float(input(f"Please enter the average number of points per game scored by the {name}: "))
-            off_rebounds = float(input(f"Please enter the average number of offensive rebounds per game earned by the {name}: "))
-            def_rebounds = float(input(f"Please enter the average number of defensive rebounds per game earned by the {name}: "))
-            ast = float(input(f"Please enter the average number of assists per game earned by the {name}: "))
-            stl = float(input(f"Please enter the average number of steals per game earned by the {name}: "))
-            blk = float(input(f"Please enter the average number of blocks per game earned by the {name}: "))
-            to = float(input(f"Please enter the average number of turnovers per game earned by the {name}: "))
-            pf = float(input(f"Please enter the average number of fouls per game earned by the {name}: "))
-        else:
-            print("That team does not exist")
+        print(teamsDict[name])
+    elif func == 3:
+        name = input("Please enter the name of the team: ")
+        p_name = input("Please enter the name of the player: ")
+        teamsDict[name].print_player(p_name)
     elif func == 0:
         """ends the program"""
         return 0
     main()
 
 def main():
-    func = int(input("Please type a number to select what you want to do.\n1:Create new team\n2:Update team\n0:Exit the program\n"))
+    func = int(input("Please type a number to select what you want to do.\n1:Create new team\n2:Display team stats\n3:Display Player Stats\n0:Exit the program\n"))
     functions(func)
 
 if __name__=="__main__":
