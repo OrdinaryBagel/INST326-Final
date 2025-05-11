7682c4624f9b577a9a643f7279932ad72f09fb87
zayan-shahab <zshahab@terpmail.umd.edu>
2025-05-11 15:44:25 -0400

    added 3 functions
    added update_player_stats within the teams class and then added a one way and 2 way trades functions.

--- Basketball_predictions.py
+++ Basketball_predictions.py
@@ -48,11 +48,11 @@ class Team:
         Adds a new player to the team.
         """
         if name in self.playersDict:
-            print(f"{name} is already on the team.")
-            return
+            return f"{name} is already on the team."
         self.playersDict[name] = Player(name, float(gp), float(pts), float(rb), float(ast), float(stl), float(blk), float(to), float(pf))
-        print(f"{name} added to {self.name}.")
         self.calc_team_total()
+        return f"{name} added to {self.name}."
+
 
     def remove_player(self,name):
         """
@@ -61,12 +61,41 @@ class Team:
         """
         if name in self.playersDict:
             del self.playersDict[name]
-            print(f"{name} removed from {self.name}.")
             self.calc_team_total()
+            return f"{name} removed from {self.name}."
         else: 
-            print(f"{name} is not on the team.")   
+            return f"{name} is not on the team."   
+        
+    def update_player_stats(self, name, gp=None, pts=None, rb=None, ast=None, stl=None, blk=None, to=None, pf=None):
+        """
+        Driver: Zayan
+        Updates the stats of an existing player on the team.
+        Only updates stats that are provided.
+        """
+        if name not in self.playersDict:
+            return f"{name} is not on the team."
 
+        player = self.playersDict[name]
 
+        if gp is not None: 
+            player.gp = float(gp)
+        if pts is not None: 
+            player.pts = float(pts)
+        if rb is not None: 
+            player.rb = float(rb)
+        if ast is not None: 
+            player.ast = float(ast)
+        if stl is not None: 
+            player.stl = float(stl)
+        if blk is not None: 
+            player.blk = float(blk)
+        if to is not None: 
+            player.to = float(to)
+        if pf is not None: 
+            player.pf = float(pf)
+
+        self.calc_team_total()
+        return f"{name}'s stats have been updated on {self.name}."
 
     def calc_team_total(self):
         """
@@ -141,6 +170,61 @@ class Player:
             f"AST={self.ast}, STL={self.stl}, BLK={self.blk}\n"
             f"TO={self.to}, PF={self.pf}\n"
         )
+
+def trade_player(team_from, team_to, player_name):
+    """
+    Driver: Zayan
+    Trades a player from one team to another.
+    This is a 1 way trade if the user wishes to move one player from a team to another.
+
+    Args:
+        team_from (Team): The team the player is currently on.
+        team_to (Team): The team the player is going to.
+        player_name (str): Name of the player being traded.
+    
+    Returns:
+        str: Message indicating the result of the trade.
+    """
+    if player_name not in team_from.playersDict:
+        return f"{player_name} is not on {team_from.name}."
+
+    if player_name in team_to.playersDict:
+        return f"{player_name} is already on {team_to.name}."
+
+    player = team_from.playersDict[player_name]
+    team_to.playersDict[player_name] = player
+    del team_from.playersDict[player_name]
+
+    team_from.calc_team_total()
+    team_to.calc_team_total()
+
+    return f"{player_name} has been traded from {team_from.name} to {team_to.name}."
+
+def trade_players(team_a, player_a_name, team_b, player_b_name):
+    """
+    Driver: Zayan
+    Trades player_a from team_a with player_b from team_b.
+    This is a 2 way trade if the user wishes to trade 2 players between 2 teams.
+    """
+    if player_a_name not in team_a.playersDict:
+        return f"{player_a_name} is not on {team_a.name}."
+    if player_b_name not in team_b.playersDict:
+        return f"{player_b_name} is not on {team_b.name}."
+    
+    player_a = team_a.playersDict[player_a_name]
+    player_b = team_b.playersDict[player_b_name]
+    
+    team_a.playersDict[player_b_name] = player_b
+    team_b.playersDict[player_a_name] = player_a
+    
+    del team_a.playersDict[player_a_name]
+    del team_b.playersDict[player_b_name]
+    
+    team_a.calc_team_total()
+    team_b.calc_team_total()
+    
+    return (f"{player_a_name} has been traded from {team_a.name} to {team_b.name}, "
+            f"and {player_b_name} has been traded from {team_b.name} to {team_a.name}.")
 
 def functions(func):
     """determines what the program will do, will be update by multiple people as the project moves forward."""
